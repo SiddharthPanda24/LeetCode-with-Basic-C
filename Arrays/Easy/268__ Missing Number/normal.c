d7d73dcbd2d87d4cbe6646553ac327261bbbd2c8
SiddharthPanda24 <siddharth.25scs1003002977@iilm.edu>
2026-09-10 00:49:32 +0530

    LeetCode 268. Missing Number

--- Arrays/Easy/268__ Missing Number/normal.c
+++ Arrays/Easy/268__ Missing Number/normal.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+    int i, n;
+
+    printf("Enter your array size:");
+    scanf("%d", &n);
+
+    int nums[n];
+
+    printf("Enter your array elements:");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int sum = 0;
+
+    for(i = 0; i < n; i++) {
+        sum += nums[i];
+    }
+
+    int total = n * (n + 1) / 2;
+
+    printf("%d", total - sum);
+
+    return 0;
+}
