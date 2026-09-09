a7d633984b8406703a4c59a9efb2fb54f7e42df7
SiddharthPanda24 <siddharth.25scs1003002977@iilm.edu>
2026-09-10 00:54:40 +0530

    LeetCode 349. Intersection of Two Arrays

--- Arrays/Easy/349__ Intersection of Two Arrays/normal.c
+++ Arrays/Easy/349__ Intersection of Two Arrays/normal.c
@@ -1,1 +1,63 @@
+#include <stdio.h>
 
+int main() {
+    int m, n;
+
+    printf("Enter your 1st array size: ");
+    scanf("%d", &m);
+
+    printf("Enter your 2nd array size: ");
+    scanf("%d", &n);
+
+    int nums1[m], nums2[n];
+
+    printf("Enter your 1st array Element: ");
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &nums1[i]);
+    }
+
+    printf("Enter your 2nd array Element: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums2[i]);
+    }
+
+    int count = 0;
+    int result[m];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+
+            if (nums1[i] == nums2[j]) {
+
+                // Check if already present in result
+                int duplicate = 0;
+
+                for (int k = 0; k < count; k++) {
+                    if (result[k] == nums1[i]) {
+                        duplicate = 1;
+                        break;
+                    }
+                }
+
+                if (duplicate == 0) {
+                    result[count] = nums1[i];
+                    count++;
+                }
+
+                break;
+            }
+        }
+    }
+
+    printf("Your final intersection array is: ");
+
+    for (int i = 0; i < count; i++) {
+        printf("%d", result[i]);
+
+        if (i < count - 1) {
+            printf(",");
+        }
+    }
+
+    return 0;
+}
